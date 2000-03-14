5f9d1d49ae1d5cf4315e07c3d3b7ba7f32d0b6fa
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2000-03-14 19:29:51 +0000

    Updated after working athome.  Major restructuring and moving stuff around.

--- SPECIALE/auto/rapport.el
+++ SPECIALE/auto/rapport.el
@@ -3,20 +3,6 @@
   (lambda ()
     (LaTeX-add-bibliographies
      "books")
-    (LaTeX-add-labels
-     "fig:#3"
-     "sec:protection-against-race-conditions"
-     "sec:providing-searching-facilities"
-     "sec:allow-fast-access-to-data")
-    (TeX-add-symbols
-     '("myimage" 3)
-     '("mycitation" 3)
-     '("tag" 1)
-     '("myurl" 2)
-     '("texcommand" 1)
-     '("ntcommand" 1)
-     '("unixcommand" 1)
-     '("framepage" 2))
     (TeX-run-style-hooks
      "graphicx"
      "verbatim"
@@ -28,17 +14,7 @@
      "latex2e"
      "bk10"
      "book"
-     "ddraft"
+     "draft"
      "a4paper"
-     "s-terms-and-concepts"
-     "s-processed-documents"
-     "s-sample-websites"
-     "s-xml"
-     "s-docbook"
-     "s-sql"
-     "s-cactus"
-     "s-konsensus"
-     "s-tex-and-latex"
-     "s-report-writing-tools"
-     "s-available-hardware-and-software"))))
+     "s-mycommands"))))
 
