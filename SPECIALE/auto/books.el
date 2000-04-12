d5e1a3df8a91211eda7e44dbe23fe3a27d81edb5
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2000-04-12 17:38:01 +0000

    Updates from today.  Added Andersesees figures, and started the final version of the Cactus chapter.

--- SPECIALE/auto/books.el
+++ SPECIALE/auto/books.el
@@ -15,5 +15,14 @@
      "knuthliterateprogramming"
      "Dybvig:1996:SPL"
      "phillipandalexsguidetowebpublishing"
-     "sewell:weaving-a-program"))))
+     "sewell:weaving-a-program"
+     "srinivasan:advanced-perl-programming"
+     "wille-koller:teach-yourself-active-server-pages-in-24-hours"
+     "vulis:modern-tex-and-its-applications"
+     "wilf:algorithms-and-complexity"
+     "webmaster-in-a-nutshell"
+     "descartes-bunce:programming-the-perl-dbi"
+     "christiansen-torkington:perl-cookbook"
+     "goosens-rahtz-mittelbach:the-latex-graphics-companion"
+     "franquin:vakse-viggo-4"))))
 
--- SPECIALE/auto/s-cactus.el
+++ SPECIALE/auto/s-cactus.el
@@ -2,7 +2,16 @@
  (function
   (lambda ()
     (LaTeX-add-labels
+     "cha:cactus"
      "sec:cactus"
+     "sec:cactus-philosophy"
+     "sec:cactus-seen-from-the-users-perspective"
+     "sec:cactus-design"
+     "sec:cactus-overall-view"
+     "sec:cactus-data-flow"
+     "sec:cactus-daemons-and-sql-tables"
+     "sec:cactus-implementation"
+     "sec:the-design-of-cactus"
      "sec:cactus-choice-of-language"
      "sec:converters"
      "sec:linux"
