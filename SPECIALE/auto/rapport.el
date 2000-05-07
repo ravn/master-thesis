e3acc1bc1a0ed5002998fa56a8f5fa252f0f9034
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2000-05-07 16:17:45 +0000

    worked all sunday

--- SPECIALE/auto/rapport.el
+++ SPECIALE/auto/rapport.el
@@ -3,17 +3,6 @@
   (lambda ()
     (LaTeX-add-bibliographies
      "books")
-    (LaTeX-add-labels
-     "sec:CGI-modperl"
-     "sec:CGI-php3"
-     "sec:CGI-servlets"
-     "sec:html-meta-tags"
-     "cha:on-demand-rendering"
-     "sec:emacs-with-psgml"
-     "sec:java-servlets"
-     "tab:mysql-examiners"
-     "fig:faqomatic-11"
-     "sec:the-importance-of-a-web-cache")
     (TeX-run-style-hooks
      "hyperref"
      "graphicx"
--- SPECIALE/auto/s-xml.el
+++ SPECIALE/auto/s-xml.el
@@ -5,7 +5,6 @@
      "cha:sgml-xml-and-dtd's"
      "tab:programming-langugages-and-html"
      "tab:distribution-formats"
-     "fig:screenshot-x-outline.xsl"
      "fig:listing-x-outline.xsl"
      "fig:src-public_html-incoming.xml"
      "fig:src-public_html-incoming.xsl"
